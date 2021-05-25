249af4a36ecb6eb805b14805a891e66783da4343
JovicAbengona <37503727+JovicAbengona@users.noreply.github.com>
2021-05-25 19:03:42 +0800

    First push for ExpressJS Survey Form assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var session = require("express-session");
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/static"));
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.get("/", function(request, response){
+    response.render("index");
+});
+
+app.post("/result", function(request, response){
+    response.render("result", {data: request.body});
+});
+
+app.listen(8080, function(){
+    console.log("Listening on 8080");
+});
